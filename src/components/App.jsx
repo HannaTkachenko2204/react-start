b4463e479fbbb64e1d23808c8d40d9fb25b0db41
Анна <anna.kuvakina91@gmail.com>
2025-06-13 23:27:06 +0300

    []+map() колекції

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,15 @@
 import Product from './Product';
+import BookList from './BookList'
+
+const favBooks = [
+  { id: "id-1", name: "JS for beginners" },
+  { id: "id-2", name: "React basics" },
+  { id: "id-3", name: "React Router overview" }
+];
 
 export default function App() {
   return (
+    <>
     <div>
       <Product
         name="Tacos With Lime"
@@ -14,5 +22,13 @@ export default function App() {
         price={14.29}
       />
     </div>
+
+    <div>
+	  <h1>Books of the week</h1>
+      <BookList books={favBooks} />
+    </div>
+
+    
+    </>
   );
 }
